d64678a9ac4dc495ab9383522c68e1672cf1cc3c
SergSukh <s9626265386@gmail.com>
2023-05-19 16:13:52 +0400

    14

--- kristall/units/urls.py
+++ kristall/units/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
 
-import view
+import views
 
 app_name = 'units'
 
 urlpatterns = [
-    path('', view.index, name='index'),
-    path('units_list/', view.units_list, name='units_list'),
-    path('units_rent/', view.units_rent, name='units_rent'),
-    path('units_sale/', view.units_sale, name='units_sale'),
-    path('unit_detail/<int:unit_id>/', view.unit_detail, name='unit_detail'),
+    path('', views.index, name='index'),
+    path('units_list/', views.units_list, name='units_list'),
+    path('units_rent/', views.units_rent, name='units_rent'),
+    path('units_sale/', views.units_sale, name='units_sale'),
+    path('unit_detail/<int:unit_id>/', views.unit_detail, name='unit_detail'),
     path(
         'unit_publicate/<int:unit_id>/',
-        view.unit_publicate,
+        views.unit_publicate,
         name='unit_publicate'
     ),
-    path('create/', view.unit_create, name='unit_create'),
-    path('m_create/', view.msg_create, name='msg_create'),
-    path('unit/<int:unit_id>/edit/', view.unit_edit, name='unit_edit'),
+    path('create/', views.unit_create, name='unit_create'),
+    path('m_create/', views.msg_create, name='msg_create'),
+    path('unit/<int:unit_id>/edit/', views.unit_edit, name='unit_edit'),
 ]
