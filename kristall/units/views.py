fc03bf2078e56adeab51821d4034521344431e1f
SergSukh <s9626265386@gmail.com>
2023-05-24 11:17:07 +0400

    123

--- kristall/units/views.py
+++ kristall/units/views.py
@@ -3,12 +3,8 @@ from datetime import datetime as dt
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import (
-    get_object_or_404,
-    get_list_or_404,
-    redirect,
-    render
-)
+from django.shortcuts import (get_object_or_404, get_list_or_404,
+                              redirect, render)
 from sorl.thumbnail import get_thumbnail
 
 from .forms import ImagesFormSet, MessageForm, UnitCreateForm, UnitForm
